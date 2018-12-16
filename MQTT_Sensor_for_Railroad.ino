37e2fdced8fe089b003e3eb990d3e33ff5247391
ntableman <nathan@tableman.com>
2018-12-16 12:54:39 -0500

    Update MQTT_Sensor_for_Railroad.ino

--- MQTT_Sensor_for_Railroad.ino
+++ MQTT_Sensor_for_Railroad.ino
@@ -42,7 +42,7 @@ String configTopic = "trains/sensors/ESP/" + WiFi.macAddress();
 
 EspMQTTClient client(
   "Mer de Glace IoT",      // ssid
-  "pencilhill",            // password
+  "",            // password
   "192.168.20.78",         // MQTT ip
   1883,                    // MQTT broker port
   "",                      // MQTT username
